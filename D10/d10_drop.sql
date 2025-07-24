623f04cdd457db9e3af0ef8b693f8c057024504e
Roberto Capancioni <roberto.capancioni@gmail.com>
2025-07-24 09:58:02 +0200

    Add files via upload

---
+++ D10/d10_drop.sql
@@ -0,0 +1,6 @@
+drop view d10_fattura_testata_vw;
+drop view d10_fattura_dettaglio_vw;
+
+drop table d10_cliente cascade constraints;
+drop table d10_fattura_testata cascade constraints;
+drop table d10_fattura_dettaglio cascade constraints;
